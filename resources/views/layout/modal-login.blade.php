6d0f869c97b572c2d5218eef5eeddd79f5e44ff8
Hoang Thanh Tung <ocsenchay1994@gmail.com>
2015-11-06 15:14:42 +0700

    Tung commit: Login awesome

---
+++ resources/views/layout/modal-login.blade.php
@@ -0,0 +1,30 @@
+@section('modal')
+  <div class="modal fade modal-login">
+    <div class="modal-dialog">
+      <div class="modal-content">
+          <div class="row fluid-container ml-content">
+            <div class="logo-profile ml-logo"></div>
+            <div class="fluid-container ml-title">Blalalal</div>
+            <div class="container ml-fblogin">
+              <div class="ml-fblogin-icon">
+                <i class="fa fa-facebook-official fa-3x"></i>
+                <div class="ml-fblogin-text">Tiếp tục với FaceBook</div>
+              </div>  
+            </div>
+            <div class="ml-line">
+              <div class="ml-line-text">hoặc</div>
+            </div>
+            <div class="fluid-container ml-formlogin">
+              <div class="form-group">
+                <input type="text" class="form-control" id="usr" placeholder='Tài khoản'>
+              </div>
+              <div class="form-group">
+                <input type="password" class="form-control" id="pwd"  placeholder='Mật khẩu'>
+              </div>
+              <button class="btn btn-primary btn-lg btn-block">Đăng nhập</button>
+            </div>
+          </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+{{-- @stop  --}}
